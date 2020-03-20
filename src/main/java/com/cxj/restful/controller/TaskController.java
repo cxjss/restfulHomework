eed3c3334555e2935ae187906e1a7766f584ffc4
cxj <chenxuanjiong123@163.com>
2020-03-20 10:40:17 +0800

    添加修改和删除方法。

--- src/main/java/com/cxj/restful/controller/TaskController.java
+++ src/main/java/com/cxj/restful/controller/TaskController.java
@@ -17,52 +17,65 @@ public class TaskController {
 
     /**
      * 查询所有
+     *
      * @return
      */
     @GetMapping("/tasks")
-    public List<Task> getTasks(){
+    public List<Task> getTasks() {
         return taskService.getTasks();
     }
 
     /**
      * 插入
+     *
      * @return
      */
     @PostMapping("/tasks")
-    public String insertTask(@RequestBody String body){
-        return "insertTask";
+    public String insertTask(@RequestBody Task task) {
+        if (taskService.insertTask(task) == 0) {
+            return "添加失败";
+        }
+        return "添加成功";
     }
 
     /**
      * 根据id查任务
+     *
      * @param id
      * @return
      */
     @GetMapping("/tasks/{id}")
-    public Task getTaskById(@PathVariable("id") Integer id){
+    public Task getTaskById(@PathVariable("id") Integer id) {
         return taskService.getTaskById(id);
     }
 
     /**
      * 删除
+     *
      * @param id
      * @return
      */
     @DeleteMapping("/tasks/{id}")
-    public String deleteTask(@PathVariable("id") String id){
-        return "deleteTask";
+    public String deleteTask(@PathVariable("id") Integer id) {
+        if (taskService.deleteTask(id) == 0) {
+            return "删除失败";
+        }
+        return "删除成功";
     }
 
     /**
      * 修改
-     * @param id
+     *
+     * @param task
      * @return
      */
-    @PutMapping("/tasks/{id}")
-    public String updateTask(@PathVariable("id") String id){
-        return "updateTask";
+    @PutMapping("/tasks")
+    public String updateTask(@RequestBody Task task) {
+        if (taskService.updateTask(task) == 0) {
+            return "修改失败";
+        }
+        return "修改成功";
     }
-
 
 
 }
--- src/main/java/com/cxj/restful/dao/impl/TaskDaoImpl.java
+++ src/main/java/com/cxj/restful/dao/impl/TaskDaoImpl.java
@@ -11,17 +11,20 @@ import org.springframework.stereotype.Repository;
 import java.io.*;
 import java.lang.annotation.Target;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository("taskDao")
 public class TaskDaoImpl implements ITaskDao {
 
+    String jsonPath = "src/main/resources/static/data.json";
     private Resource resource = new ClassPathResource("static/data.json");
 
-    public void writeJson(String path, String str){
-        PrintStream stream=null;
+    public void writeJson(String path, List<Task> list) {
+        String str = JSON.toJSONString(list);
+        PrintStream stream = null;
         try {
-            stream=new PrintStream(path);//写入的文件path
+            stream = new PrintStream(path);//写入的文件path
             stream.print(str);//写入的字符串
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -30,13 +33,13 @@ public class TaskDaoImpl implements ITaskDao {
 
     @Override
     public List<Task> getTasks() {
-        if(resource.exists()){
+        if (resource.exists()) {
             try {
                 InputStream is = resource.getInputStream();
                 BufferedReader br = new BufferedReader(new InputStreamReader(is));
                 StringBuffer sb = new StringBuffer();
                 String line;
-                while ((line=br.readLine())!=null){
+                while ((line = br.readLine()) != null) {
                     sb.append(line);
                 }
                 List<Task> tasks = JSON.parseArray(sb.toString(), Task.class);
@@ -52,13 +55,12 @@ public class TaskDaoImpl implements ITaskDao {
     public int insertTask(Task task) {
         List<Task> list = getTasks();
         for (Task originTask : list) {
-            if(originTask.getId() == task.getId()){
+            if (originTask.getId() == task.getId()) {
                 return 0;
             }
         }
         list.add(task);
-        String res = JSON.toJSONString(list);
-        writeJson("src/main/resources/static/data.json",res);
+        writeJson(jsonPath,list);
         return 1;
     }
 
@@ -66,7 +68,7 @@ public class TaskDaoImpl implements ITaskDao {
     public Task getTaskById(int id) {
         List<Task> list = getTasks();
         for (Task task : list) {
-            if(task.getId() == id){
+            if (task.getId() == id) {
                 return task;
             }
         }
@@ -75,11 +77,28 @@ public class TaskDaoImpl implements ITaskDao {
 
     @Override
     public int deleteTask(int id) {
+        List<Task> list = getTasks();
+        for (Task originTask : list) {
+            if (originTask.getId() == id) {
+                list.remove(originTask);
+                writeJson(jsonPath,list);
+                return 1;
+            }
+        }
         return 0;
     }
 
     @Override
     public int updateTask(Task task) {
+        List<Task> list = getTasks();
+        for (Task originTask : list) {
+            if (originTask.getId() == task.getId()) {
+                originTask.setContent(task.getContent());
+                originTask.setCreatedTime(task.getCreatedTime());
+                writeJson(jsonPath,list);
+                return 1;
+            }
+        }
         return 0;
     }
 }
